6337dde03938150b3de1c28b3ea74628ec8d86a2
Sivanaque <me@antoinespiteri.net>
2024-02-22 16:00:26 +0000

    Thu, Feb 22, 2024, 5:00 PM +01:00

--- struc.c
+++ struc.c
@@ -9,14 +9,17 @@ struct bus {
 };
 
 int main() {
-
     int nb = 0;
     int i;
-    struct bus prop[nb];
 
     printf("Combien souhaitez-vous ajouter de bus ?\n");
     scanf("%d", &nb);
 
+    struct bus *prop = malloc(nb * sizeof(struct bus));
+    if (prop == NULL) {
+        printf("Erreur d'allocation de mémoire\n");
+        return 1;
+    }
 
     for (i = 0; i < nb; i++) {
         printf("\nEntrer l'ID du bus %d : ", i);
@@ -29,51 +32,13 @@ int main() {
         scanf ("%d", &prop[i].seats);
 
         printf("\nEnter le nom du chauffeur de bus %d : ", i);
-        scanf ("%50s", prop[i].driver);
+        scanf ("%s", prop[i].driver);
     }
 
     for (i = 0; i < nb; i++) {
         printf("\nID : %d \nLigne : %d \nPlaces : %d \nConducteur : %s", prop[i].id, prop[i].line, prop[i].seats, prop[i].driver);
     }
-  return 0;
-}
-/*
 
-struct details student[3] = {    <---- Array de structure variable
-    {
-      "Aisiri","A",89.6
-    },
-    {
-      "Gourav","B",60.4
-    },
-    {
-      "Samuel","C",98.4
-    },
-  };
-  print_struct(student);
-
-
-
-#include<stdio.h>
-#include <string.h>
-struct student{
-int rollno;
-char name[10];
-};
-int main(){
-int i;
-struct student st[5];
-printf("Enter Records of 5 students");
-for(i=0;i<5;i++){
-printf("\nEnter Rollno:");
-scanf("%d",&st[i].rollno);
-printf("\nEnter Name:");
-scanf("%s",st[i].name);
+    free(prop); // N'oubliez pas de libérer la mémoire allouée dynamiquement
+    return 0;
 }
-printf("\nStudent Information List:");
-for(i=0;i<5;i++){
-printf("\nRollno:%d, Name:%s",st[i].rollno,st[i].name);
-}
-   return 0;
-}
-*/
