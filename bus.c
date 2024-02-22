f0f55511da170480c0daddd14397eb383c0708f4
Sivanaque <me@antoinespiteri.net>
2024-02-22 14:56:27 +0000

    Thu, Feb 22, 2024, 3:56 PM +01:00

--- bus.c
+++ bus.c
@@ -1,46 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct details {
-  char name[20];
-  char sec[20];
-  float per;
+int menu(void);
+struct bus ajout_bus();
+int nbbus;
+
+struct bus {
+    int id;
+    int seats;
+    int line;
+    char driver[50];
 };
 
-void print_struct(struct details str_arr[]);
+struct ligne {
+    int number;
+    int length;
+    int nbstations;
+    int minbus;
+    int maxbus;
+};
 
-int main() {
-  struct details student[3] = {
-    {
-      "Aisiri","A",89.6
-    },
-    {
-      "Gourav","B",60.4
-    },
-    {
-      "Samuel","C",98.4
-    },
-  };
-  print_struct(student);
-  return 0;
+struct station {
+    int nubmer;
+    char name[40];
+    int line;
+};
+
+int main(void) {
+    int choix = menu();
+
+    return 0;
 }
 
-/* void print_struct(struct details str_arr[]) {
-  int i;
+struct bus ajout_bus()
+{
+  struct bus 13;
 
-  for (i = 0; i < 3; i++) {
-    printf("Name: %s\n", str_arr[i].name);
-    printf("Section: %s\n", str_arr[i].sec);
-    printf("Percentage: %.2f\n", str_arr[i].per);
-    printf("\n");
-  }
-} */
+  printf("Entrer l'ID du bus : ");
+  scanf ("%d", &bus1.id);
 
-void ajout_bus() {
-    int nb;
+  printf("Entrer la ligne du bus : ");
+  scanf ("%d", &bus1.line);
+
+  printf("Entrer le nombre de places disponibles du bus : ");
+  scanf ("%d", &bus1.seats);
+
+  printf("Enter le nom du chauffeur de bus : ");
+  scanf (" %[^\n]%*c", bus1.driver);
+
+  return bus1;
+}
+
+int menu(void) {
+    int nbus;
+    int choix;
     do {
-        printf("Combien souhaitez-vous ajouter de bus ?");
-        scanf("%d", nb);
-    } while (nb = 0);
 
+
+        printf("#### MENU ####\n");
+        printf("1 - Gérer bus\n");
+        printf("2 - Gérer ligne\n");
+        printf("3 - Statistiques\n");
+        printf("0 - Quitter\n");
+        printf("Votre choix : ");
+
+        scanf("%d", &choix);
+
+        switch(choix) {
+            case 1:
+                printf("1 - Ajouter un bus\n");
+                printf("2 - Afficher\n");
+                printf("4 - Supprimer\n");
+                printf("5 - Afficher Chauffeurs\n");
+                printf("6 - Trier par marque\n");
+                printf("7 - Trier par nombre de places\n");
+                printf("0 - Quitter\n");
+                printf("Votre choix : ");
+
+                scanf("%d", &choix);
+                if (choix == 1) {
+                    struct bus rbus;
+                    rbus = ajout_bus();
+                    printf("\n RECAPULATIF \n");
+                    printf("\nID du bus : %d", rbus.id);
+                    printf("\nLigne du bus : %d", rbus.line);
+                    printf("\nPlaces disponibles dans le bus : %d", rbus.seats);
+                    printf("\nNom du chauffeur : %s\n", rbus.driver);
+
+                }
+                break;
+            case 2:
+                printf("1 - Ajouter\n");
+                printf("2 - Afficher tous\n");
+                printf("3 - Consulter\n");
+                printf("4 - Supprimer\n");
+                printf("5 - Afficher bus d'une ligne\n");
+                printf("6 - Trier par nombres de stations\n");
+                printf("0 - Quitter\n");
+
+                scanf("%d", &choix);
+                break;
+           case 3:
+                printf("1 - Min bus par ligne\n");
+                printf("2 - Max bus par ligne\n");
+                printf("3 - Moyenne bus par ligne\n");
+                printf("0 - Quitter\n");
+
+                scanf("%d", &choix);
+                break;
+            }
+        } while (choix != 0);
+
+     return choix;
 }
--- struc.c
+++ struc.c
@@ -3,19 +3,57 @@
 
 struct bus {
     int id;
+    int seats;
     int line;
-    int place;
+    char driver[50];
 };
 
-int main(void) {
-    char caca[10] = "salut";
-    struct bus caca;
+void print_struct(struct details str_arr[]);
 
-    caca.id = 192834;
-    caca.line = 4;
-    caca.place = 68;
+int main() {
 
-    printf("%d \n%d \n%d", caca.id, caca.line, caca.place);
+  return 0;
+}
+/*
+
+struct details student[3] = {    <---- Array de structure variable
+    {
+      "Aisiri","A",89.6
+    },
+    {
+      "Gourav","B",60.4
+    },
+    {
+      "Samuel","C",98.4
+    },
+  };
+  print_struct(student);
+
+*/
+/* void print_struct(struct details str_arr[]) {    <--- AFFICHER RESULTAT
+  int i;
+
+  for (i = 0; i < 3; i++) {
+    printf("Name: %s\n", str_arr[i].name);
+    printf("Section: %s\n", str_arr[i].sec);
+    printf("Percentage: %.2f\n", str_arr[i].per);
+    printf("\n");
+  }
+} */
+
+void ajout_bus() {
+    int nb = 0;
+    int i;
+    do {
+        printf("Combien souhaitez-vous ajouter de bus ?");
+        scanf("%d", nb);
+    } while (nb = 0);
+
+    struct details bus[nb] = {
+        {
+            
+        }
+    }
+
 
 }
-
